2be9e1dad56af07199a1b79c70dae3c5289abd3b
Bear <world.life.destiny@icloud.com>
2023-12-26 12:03:24 +0300

    new

---
+++ src/main/java/ru/skillfactory/HelloController.java
@@ -0,0 +1,16 @@
+package ru.skillfactory;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+    @RequestMapping(value = "/student", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloStudent() {
+        return "Hello, student!";
+    }
+}
