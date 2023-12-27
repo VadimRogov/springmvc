7f092e032637e8ddef00b60d97ce9f0f0bcf5777
LiS <world.life.destiny@icloud.com>
2023-12-28 00:56:28 +0300

    version1

--- src/main/java/ru/skillfactory/dto/StudentData.java
+++ src/main/java/ru/skillfactory/dto/StudentData.java
@@ -1,36 +1,20 @@
 package ru.skillfactory.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import java.io.Serializable;
 
-public class StudentData {
+@Data
+public class StudentData implements Serializable {
+
     @JsonProperty("firstName")
     private String firstName;
+
     @JsonProperty("lastName")
     private String lastName;
-    @JsonProperty("")
+
+    @JsonProperty("grade")
     private int grade;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getGrade() {
-        return grade;
-    }
-
-    public void setGrade(int grade) {
-        this.grade = grade;
-    }
 }
+
+
